491390c5fecd1ec64e6d651b7bc2d6b2891551c7
HerrLinks <atti.szucs85@gmail.com>
2023-08-09 19:21:00 +0200

    candisPerSEEcound fixed

--- script.ts
+++ script.ts
@@ -36,7 +36,7 @@ function buyLollipop() {
 
 function candysPerSecond() {
   if (lollipopCounter.innerHTML.length >= 10) {
-    candy += speed;
+    candy += lollipopCounter.innerHTML.length;
     candyCounter.innerHTML = candy.toString();
   }
 }
